b9bb974af340665c6f64b5fceab87dc5166cfea6
ryan.lynch <ryan.lynch14@yahoo.com>
2024-08-13 12:59:45 -0400

    Refactored paths to be relative instead of absolute for improved portability and flexibility.

--- Fleet_Invoice_Email/fleet_constants.py
+++ Fleet_Invoice_Email/fleet_constants.py
@@ -1,7 +1,8 @@
+import os
 ### Only Change this line ###
 month = 'September'
 month_folder = 'AUGUST 2023 SALES'
-image_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Invoices\Misty\Company Logo.png"
+image_path = os.path.join(os.path.dirname(__file__), 'Company Logo.png')
 signature = f'''
     <br><span style="font-family:'Bradley Hand ITC', cursive, sans-serif; color: #0C1731; font-size: 16pt;">Misty Douglas<br></span>
     <i>Accounts Receivable (Fleet)</i><br><br>
@@ -13,8 +14,7 @@ signature = f'''
     <img src="{image_path}" alt="Company Logo" style="width: 10px; height: auto;">
     '''
 
-# folder_path = rf"C:\Users\MistyDouglas\OneDrive - Tidal Wave Autospa\Documents\ACCOUNTS RECEIVABLE\FLEET\HOME OFFICE\PDFs to EMAIL\{month_folder}"
-folder_path = rf'C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Scripts\PyTest'
+folder_path = os.path.join(os.path.dirname(__file__), 'Invoices', month_folder)
 exclude_domain = "fleetbilling@tidalwaveautospa.com"
 email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 ##################################
