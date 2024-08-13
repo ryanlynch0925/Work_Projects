816b6974ee478798e083a7f1bc0a9fabf41225dd
ryan.lynch <ryan.lynch14@yahoo.com>
2024-08-13 10:24:17 -0400

    Reconfigured structure, updated code from local file
    	deleted:    Over-45-Days-Old-Report/README.md
    	deleted:    Over-45-Days-Old-Report/config.py
    	deleted:    Over-45-Days-Old-Report/pyvenv.cfg
    	new file:   Over_45_Days_Old/.gitignore
    	renamed:    Over-45-Days-Old-Report/CONTRIBUTING.md -> Over_45_Days_Old/CONTRIBUTING.md
    	renamed:    Over-45-Days-Old-Report/LICENSE.txt -> Over_45_Days_Old/LICENSE.txt
    	renamed:    Over-45-Days-Old-Report/over45dayssoldreport.py -> Over_45_Days_Old/Over 45 Days Report.py
    	new file:   Over_45_Days_Old/Over 45 Email.py
    	renamed:    Over-45-Days-Old-Report/READme.md -> Over_45_Days_Old/READme.md
    	new file:   Over_45_Days_Old/config.py
    	renamed:    Over-45-Days-Old-Report/requirements.txt -> Over_45_Days_Old/requirements.txt

--- Over-45-Days-Old-Report/config.py
+++
@@ -1,2 +0,0 @@
-data_path = r'c:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Test.xlsx'
-summary_file_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\30-Day Report\45-Day Report.xlsx"
---
+++ Over_45_Days_Old/Over 45 Email.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from datetime import datetime
+import win32com.client as win32
+import traceback
+from config import signature
+
+report_name = '8.5.24'
+workbook_path = f"C:\\Users\DavidLynch\\OneDrive - Tidal Wave Autospa\\Documents\\30-Day Report\\{report_name}.xlsb"
+
+df = pd.read_excel(workbook_path)
+
+def initialize_outlook():
+    try:
+        outlook = win32.Dispatch('Outlook.Application')
+        return outlook
+    except Exception as e:
+        print(f'Error occured while initializing Outlook: {e}')
+        traceback.print_exc()
+
+def clean_and_filter(df):
+    filtered_df = df[df['Email'] == 'Yes']
+    # filtered_df = df[df['Not Submitted'] >= 50.00]
+    return filtered_df
+
+def gather_corrections_data(clean_filterd_df):
+    unique_employees = clean_filterd_df.groupby(['Employee', 'Email'])
+    return unique_employees
+
+def create_email(outlook, unique_employees):
+    
+    for (employee, email), group in unique_employees:
+        manager = group.iloc[0]['Manager Email']
+        employee = group.iloc[0]['Employee']
+        employee_email = group.iloc[0]['Employee Email']
+        not_submitted = group.iloc[0]['Not Submitted (Total)']
+        today = datetime.today().strftime("%A %B %d")
+        email = outlook.CreateItem(0)  # Create an Outlook email object
+        
+        # Set up the email content
+        email.To = employee_email
+        email.CC = manager
+        email.BCC = '; Karla Kendrick'
+        email.Subject = "Expenses Over 45 Days Old"
+        email.HTMLBody = '<p style="color:red; text-decoration:underline;">THIS EMAIL IS BEING SENT ON BEHALF OF MARLAN NICHOLS:</p><br>'
+        email.HTMLBody += f"Dear {employee},<br><br>"
+        email.HTMLBody += f'''
+        As of {today}, our records indicated that you have credit card charges over 45 days totaling <b>---${not_submitted:,.2f}---</b> that have not been submitted in the Workday T&E system for approval by your supervisor.  Tidal Wave company policy provides that employees with unsubmitted expenses over 30 days may result in a suspension of your company credit card.  This email is to alert you that you have <u><i>5 days</i></u> to submit your delinquent charges for processing.   If you still have delinquent charges in <u><i>5 days</i></u>, your company credit card will be suspended until your expense reporting is brought current.<br><br>
+        '''
+        email.HTMLBody += 'If you have questions on your outstanding credit card charges, please reach out to David Lynch.<br>'
+        email.HTMLBody += signature
+        # email.Display()
+        # break
+        email.Send()
+
+outlook = initialize_outlook()
+clean_filterd_df = clean_and_filter(df)
+unique_employees = gather_corrections_data(clean_filterd_df)
+email = create_email(outlook, unique_employees)
