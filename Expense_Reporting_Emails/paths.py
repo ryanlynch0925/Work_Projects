d3ff1b4a3c8bce187bea479aa49647ba53309976
ryan.lynch <ryan.lynch14@yahoo.com>
2024-08-13 14:04:37 -0400

    Refactored paths to be relative instead of absolute for improved portability and flexibility.

--- Expense_Reporting_Emails/paths.py
+++ Expense_Reporting_Emails/paths.py
@@ -1,7 +1,10 @@
-cheatsheet_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\.000001 TW Expenses\Expense Item Cheat Sheat.docx"
-itemization_instructions_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\.1 Training\Itemizing Receipts.docx"
-amazon_invoice_error_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\PDF Instructions\Amazon_Order_Instructions.pdf"
-walmart_dot_com_invoice_error_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\PDF Instructions\Walmart_Order_Instructions.pdf"
-globalIndustrial_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\.1 Training\GlobalIndustrial.pdf"
+import os
+
+cheatsheet_path = os.path.join(os.path.dirname(__file__), "Training", "Expense Item Cheat Sheat.docx")
+itemization_instructions_path = os.path.join(os.path.dirname(__file__), "Training", "Itemizing Receipts.docx")
+amazon_invoice_error_path = os.path.join(os.path.dirname(__file__), "Training", "Amazon_Order_Instructions.pdf")
+walmart_dot_com_invoice_error_path = os.path.join(os.path.dirname(__file__), "Training", "Walmart_Order_Instructions.pdf")
+globalIndustrial_path = os.path.join(os.path.dirname(__file__), "Training", "GlobalIndustrial.pdf")
 data_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Corrections Template.xlsx"
-image_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Scripts\Company Logo.png"
+image_path = os.path.join(os.path.dirname(__file__), "Company Logo.png")
+correction_data_path = os.path.join(os.path.dirname(__file__), "Corrections V2.0.xlsx")
