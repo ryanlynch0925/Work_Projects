96a7c2633b84cd74d3e93a407e9b3faa8379cac5
ryan.lynch <ryan.lynch14@yahoo.com>
2024-08-16 10:26:48 -0400

    Refactored file paths to make it more portable in the constants file.

--- Personal_Expense_Deduction_Email/personal_expense_constants.py
+++ Personal_Expense_Deduction_Email/personal_expense_constants.py
@@ -2,10 +2,9 @@ import pandas as pd
 from datetime import datetime
 
 # File and Sheet Variables
-file_name = 'Personal Expenses.xlsx'
-excel_file = rf"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Personal Reimbursements\{file_name}"
-sheet_name = input("What is the sheet name?")
-image_path = r"C:\Users\DavidLynch\OneDrive - Tidal Wave Autospa\Documents\Scripts\Company Logo.png"
+excel_file = 'Personal Expenses.xlsx'
+sheet_name = input("What is the sheet name?\n")
+image_path = "../Company Logo.png"
 
 signature = f'''
             <br><span style= 'color: #E476F44; font-size: 22pt'>David Ryan Lynch</b><br></span>
